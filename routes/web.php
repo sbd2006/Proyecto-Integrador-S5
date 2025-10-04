75a9b0761c95b9ac1653cb85051e89ba150398d5
Santi104 <monzonedi05@gmail.com>
2025-10-04 17:11:00 -0500

    Planteamiento del login mas las url de categoria, producto y PDF

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    //ruta categorias
+    Route::resource('/categoria', CategoriaController::class);
+    //ruta productos
+    Route::resource('/producto', ProductoController::class);
+    //pdf
+    Route::get('/pdfProductos', [PdfController::class, 'pdfProductos']) -> name('pdf.productos');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
