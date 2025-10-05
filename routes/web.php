c7bbef7679819de8638da0b70d1e881379deca2d
Santi104 <monzonedi05@gmail.com>
2025-10-04 19:44:23 -0500

    Se agrego la plantilla de la empresa unida junto al login y el dashboard de laravel

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductoController;
+use App\Http\Controllers\CategoriaController;
+use App\Http\Controllers\PdfController;
+
 
 Route::get('/', function () {
     return view('welcome');
